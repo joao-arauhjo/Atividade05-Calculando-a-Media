023cfd6bc672a6862f0ff654bffd78ca569c8962
JohnAra <87151137+joao-arauhjo@users.noreply.github.com>
2021-08-18 23:40:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//Exercício 5 - Elabore um programa que receba 3 notas de um aluno e calcule a média dele.//
+let nota1 = prompt("Digite sua 1ª nota: ");
+let nota2 = prompt("Digite sua 2ª nota: ");
+let nota3 = prompt("Digite sua 3ª nota: ");
+let resultado = (Number(nota1))+(Number(nota2))+(Number(nota3))
+let media = resultado / 3
+let final = console.log("Sua média é: " +media)
